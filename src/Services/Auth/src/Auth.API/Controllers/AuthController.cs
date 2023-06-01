23249f36e85815e667c53e8f07bedfaa9e5ba3c5
schierke444 <mafty.erin01@gmail.com>
2023-06-01 11:36:02 +0800

    add refresh user token

--- src/Services/Auth/src/Auth.API/Controllers/AuthController.cs
+++ src/Services/Auth/src/Auth.API/Controllers/AuthController.cs
@@ -109,4 +109,38 @@ public class AuthController : BaseController
         }
     }
 
+    [HttpGet("refresh")]
+    public async Task<ActionResult<AuthDetailsDto>> RefreshUserToken()
+    {
+        try
+        {
+            var cookie = Request.Cookies["rt"];
+            if(cookie == null || !_jwtService.VerifyRefreshToken(cookie, out string userId))
+            {
+                return Unauthorized("User Unauthorized.");
+            }
+
+            var user = await _context.Users.FirstOrDefaultAsync(x => x.Id.ToString() == userId);
+            if(user == null)
+                return Unauthorized("User was not found.");
+
+            AuthDetailsDto authDetails = new()
+            {
+                Id = user.Id,
+                Username = user.Username,
+                AccessToken = _jwtService.GenerateJwt(user.Id, false)
+            };
+
+            Response.Cookies.Append("rt", _jwtService.GenerateJwt(user.Id, true), new CookieOptions
+            {
+                HttpOnly = true,
+                MaxAge = TimeSpan.FromDays(7)
+            });
+            return Ok(authDetails); 
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, new {message = ex.Message});
+        }
+    }
 }
