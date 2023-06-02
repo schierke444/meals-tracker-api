7bad284404a96ff99e7fda7c0d6cff422226383c
schierke444 <mafty.erin01@gmail.com>
2023-06-02 16:45:07 +0800

    add repository library

---
+++ src/BuildingBlocks/Commons/Interfaces/IRepositoryBase.cs
@@ -0,0 +1,14 @@
+using System.Linq.Expressions;
+
+namespace BuildingBlocks.Commons.Interfaces;
+
+public interface IRepositoryBase<T>
+{
+    Task<T?> GetValue(Expression<Func<T, bool>> expression, bool AsNoTracking = true);
+    Task<TResponse?> GetValue<TResponse>(Expression<Func<T, bool>> expression, Expression<Func<T, TResponse>> selector, bool AsNoTracking = true);
+    Task<IEnumerable<T>> GetAllValues(Expression<Func<T, bool>>? expression = null, bool AsNoTracking = true);
+    Task<IEnumerable<T>> GetAllValues(Expression<Func<T, bool>>? expression = null, List<Expression<Func<T, object>>>? includes = null, bool AsNoTracking = true);
+    Task Create(T entity);
+    void Delete(T entity);
+    Task SaveChangesAsync(CancellationToken cancellationToken = default);
+}
