382df4a740277265edb9c5dfbae6cfa98c6422c7
schierke444 <mafty.erin01@gmail.com>
2023-06-04 06:48:15 +0800

    add mealingredients service

---
+++ src/Services/MealIngredients/src/MealIngredients.API/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using BuildingBlocks.EFCore;
+using MealIngredients.API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace MealIngredients.API.Persistence;
+
+public sealed class ApplicationDbContext : ApplicationDbContextBase
+{
+    public DbSet<MealIngredient> MealIngredients => Set<MealIngredient>();
+    private readonly IConfiguration _config;
+
+    public ApplicationDbContext(IConfiguration config)
+    {
+        _config = config;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql(_config["ConnectionStrings:DB"]);
+        base.OnConfiguring(optionsBuilder);
+    }
+}
---
+++ src/Services/MealIngredients/src/MealIngredients.API/Profiles/MealIngredientsProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using BuildingBlocks.Commons.Models.EventModels;
+using BuildingBlocks.Events;
+using MealIngredients.API.Entities;
+
+namespace MealIngredients.API.Profiles;
+
+public sealed class MealIngredientsProfile : Profile
+{
+    public MealIngredientsProfile()
+    {
+        CreateMap<CreateMealEventDto, MealIngredient>()
+            .ForMember(dest => dest.CreatedAt, src => src.MapFrom(x => DateTime.UtcNow))
+            .ForMember(dest => dest.UpdatedAt, src => src.MapFrom(x => DateTime.UtcNow));
+
+    }
+}
