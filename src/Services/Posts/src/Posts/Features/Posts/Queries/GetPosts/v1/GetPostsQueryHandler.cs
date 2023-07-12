7c158223edce7ccb49dbc4a4ae82f46eba40b28d
syazell1 <mafty.erin01@gmail.com>
2023-07-12 09:24:16 +0800

    add get all posts handler

--- src/Services/Posts/src/Posts/Features/Posts/Queries/GetPosts/v1/GetPostsQueryHandler.cs
+++ src/Services/Posts/src/Posts/Features/Posts/Queries/GetPosts/v1/GetPostsQueryHandler.cs
@@ -1,13 +1,27 @@
 using BuildingBlocks.Commons.CQRS;
 using BuildingBlocks.Commons.Models;
 using Posts.Features.Posts.Dtos;
+using Posts.Features.Posts.Interfaces;
 
 namespace Posts.Features.Posts.Queries.GetPosts.v1;
 
 sealed class GetPostsQueryHandler : IQueryHandler<GetPostsQuery, PaginatedResults<PostDetailsDto>>
 {
-    public Task<PaginatedResults<PostDetailsDto>> Handle(GetPostsQuery request, CancellationToken cancellationToken)
+    private readonly IPostRepository _postsRepository;
+    public GetPostsQueryHandler(IPostRepository postsRepository)
     {
-        throw new NotImplementedException();
+        _postsRepository = postsRepository;
+    }
+    public async Task<PaginatedResults<PostDetailsDto>> Handle(GetPostsQuery request, CancellationToken cancellationToken)
+    {
+        var results = await _postsRepository.GetAllPosts(
+            request.Search,
+            request.SortColumn,
+            request.SortOrder,
+            request.Page,
+            request.PageSize
+        );
+
+        return results;
     }
 }
