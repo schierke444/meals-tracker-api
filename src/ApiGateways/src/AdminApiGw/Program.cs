c151374f50b896edccb5654493ebb6ea1018408e
syazell1 <mafty.erin01@gmail.com>
2023-07-13 06:00:16 +0800

    add api gateways

--- src/ApiGateways/src/AdminApiGw/Program.cs
+++ src/ApiGateways/src/AdminApiGw/Program.cs
@@ -9,20 +9,18 @@ builder.Services.AddReverseProxy()
 builder.Services.AddJwtExtensions(builder.Configuration);
 
 builder.Services.AddAuthorization(opt => {
-    opt.AddPolicy("memberPolicy", policy => {
+    opt.AddPolicy("adminPolicy", policy => {
         policy.RequireAuthenticatedUser()
-            .RequireRole("Member");
+            .RequireRole("Admin");
     });
 });
+
 
 builder.Host.UseSerilog((ctx, cfg) => cfg.ReadFrom.Configuration(ctx.Configuration));
 
 var app = builder.Build();
 
 app.UseSerilogRequestLogging();
-
-app.UseAuthentication();
-app.UseAuthorization();
 
 app.MapReverseProxy();
 
