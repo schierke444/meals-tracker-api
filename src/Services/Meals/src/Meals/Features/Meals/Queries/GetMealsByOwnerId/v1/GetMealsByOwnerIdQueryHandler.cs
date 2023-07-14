43a35d295736f4d5bc16b911e183c37068ced637
syazell1 <mafty.erin01@gmail.com>
2023-07-14 19:24:01 +0800

    update meals query

--- src/Services/Meals/src/Meals/Features/Meals/Queries/GetMealsByOwnerId/v1/GetMealsByOwnerIdQueryHandler.cs
+++ src/Services/Meals/src/Meals/Features/Meals/Queries/GetMealsByOwnerId/v1/GetMealsByOwnerIdQueryHandler.cs
@@ -1,5 +1,7 @@
 using BuildingBlocks.Commons.CQRS;
 using BuildingBlocks.Commons.Models;
+using BuildingBlocks.Events.Users;
+using MassTransit;
 using Meals.Commons.Interfaces;
 using Meals.Features.Meals.Dtos;
 
@@ -8,15 +10,19 @@ namespace Meals.Features.Meals.Queries.GetMealsByOwnerId.v1;
 sealed class GetMealsByOwnerIdQueryHandler : IQueryHandler<GetMealsByOwnerIdQuery, PaginatedResults<MealsDto>>
 {
     private readonly IMealsRepository _mealsRepository;
-    public GetMealsByOwnerIdQueryHandler(IMealsRepository mealsRepository)
+    private readonly IRequestClient<GetUserByIdRecord> _client;
+    public GetMealsByOwnerIdQueryHandler(IMealsRepository mealsRepository, IRequestClient<GetUserByIdRecord> client)
     {
         _mealsRepository = mealsRepository;
+        _client = client;
     }
 
     public async Task<PaginatedResults<MealsDto>> Handle(GetMealsByOwnerIdQuery request, CancellationToken cancellationToken)
     {
+        var user = await _client.GetResponse<GetUserByIdResult>(new GetUserByIdRecord(request.OwnerId));
+
         var results = await _mealsRepository.GetPagedMealsListByOwnerId(
-            request.OwnerId, 
+            user.Message.Id.ToString(),
             request.Search,
             request.SortColumn,
             request.SortOrder,
